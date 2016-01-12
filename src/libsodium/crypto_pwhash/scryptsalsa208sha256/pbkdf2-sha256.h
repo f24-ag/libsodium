f03d97e029a35170a5a13440feb1ec0ed901b116
audrius <audrius@f24.com>
2016-01-12 14:23:13 +0100

    Exposed PBKDF2_SHA256 hash function - it is used by TrustCase web login.

--- src/libsodium/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.h
+++ src/libsodium/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.h
@@ -39,6 +39,7 @@
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
+SODIUM_EXPORT
 void PBKDF2_SHA256(const uint8_t *, size_t, const uint8_t *, size_t,
                    uint64_t, uint8_t *, size_t);
 
