224f4915f24d4e4df4bce055562acc989f18b611
audrius <audrius@f24.com>
2018-10-23 12:53:42 +0200

    Added include which has gone missing during merge

--- src/libsodium/include/sodium/crypto_pwhash.h
+++ src/libsodium/include/sodium/crypto_pwhash.h
@@ -140,6 +140,15 @@ SODIUM_EXPORT
 const char *crypto_pwhash_primitive(void)
             __attribute__ ((warn_unused_result));
 
+/**
+ * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
+ * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
+ */
+SODIUM_EXPORT
+void PBKDF2_SHA256(const uint8_t *, size_t, const uint8_t *, size_t, uint64_t,
+                   uint8_t *, size_t);
+
 #ifdef __cplusplus
 }
 #endif
